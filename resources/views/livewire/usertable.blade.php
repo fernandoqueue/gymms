f1787f30a4b68ec54967d7a92478846d3f4445b4
fernandoqueue <fernando_q89@yahoo.com>
2022-01-14 06:12:45 -0800

    Added user impersonation

--- resources/views/livewire/usertable.blade.php
+++ resources/views/livewire/usertable.blade.php
@@ -109,27 +109,7 @@
         </div>
         <div class="col-6">
             <div style="float: right;">
-
-               @if ($paginator->hasPages())
-                    <h1>Test</h1>
-                    <nav>
-                        <ul class="pagination">
-                            {{-- Previous Page Link --}}
-                            @if ($paginator->onFirstPage())
-                                <li class="disabled" aria-disabled="true"><span>@lang('pagination.previous')</span></li>
-                            @else
-                                <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">@lang('pagination.previous')</a></li>
-                            @endif
-
-                            {{-- Next Page Link --}}
-                            @if ($paginator->hasMorePages())
-                                <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">@lang('pagination.next')</a></li>
-                            @else
-                                <li class="disabled" aria-disabled="true"><span>@lang('pagination.next')</span></li>
-                            @endif
-                        </ul>
-                    </nav>
-                @endif
+                {{ $users->links('vendor.pagination.simple-defalt') }}
             </div>
         </div>
     </div>
