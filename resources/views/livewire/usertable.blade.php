dbfb787daefe579e7d8b15f073844c13b80a1a58
fernandoqueue <fernando_q89@yahoo.com>
2022-01-14 06:04:02 -0800

    Added user impersonation

--- resources/views/livewire/usertable.blade.php
+++ resources/views/livewire/usertable.blade.php
@@ -48,7 +48,7 @@
                         </tr>
                     </thead>
                     <tbody wire:loading.delay style="border-top: unset;">
-                        <tr>
+                        <tr >
                             <td style="background-color:white; text-align: center;" colspan="10">Loading...</td>
                         </tr>
                     </tbody>
@@ -109,7 +109,27 @@
         </div>
         <div class="col-6">
             <div style="float: right;">
-                {{ $users->links() }}
+
+               @if ($paginator->hasPages())
+                    <h1>Test</h1>
+                    <nav>
+                        <ul class="pagination">
+                            {{-- Previous Page Link --}}
+                            @if ($paginator->onFirstPage())
+                                <li class="disabled" aria-disabled="true"><span>@lang('pagination.previous')</span></li>
+                            @else
+                                <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">@lang('pagination.previous')</a></li>
+                            @endif
+
+                            {{-- Next Page Link --}}
+                            @if ($paginator->hasMorePages())
+                                <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">@lang('pagination.next')</a></li>
+                            @else
+                                <li class="disabled" aria-disabled="true"><span>@lang('pagination.next')</span></li>
+                            @endif
+                        </ul>
+                    </nav>
+                @endif
             </div>
         </div>
     </div>
