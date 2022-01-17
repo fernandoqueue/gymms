bc1b411013ef700c2af5ea61d59d964afcb21d60
fernandoqueue <fernando_q89@yahoo.com>
2022-01-17 15:21:10 -0800

    Added Roles and Permission to the tenant

--- database/seeders/tenant/DatabaseSeeder.php
+++ database/seeders/tenant/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Role;
+use App\Models\User;
+use App\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,16 +14,68 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            'Database\Seeders\Tenant\PermissionsTableSeeder',
-            PermissionsTableSeeder::class,
-            RolesTableSeeder::class,
-            PermissionRoleTableSeeder::class,
-            UsersTableSeeder::class,
-            RoleUserTableSeeder::class,
-        ]);
+        
+        //Permissions
+        $permissions = [
+            [
+                'id'         => '1',
+                'title'      => 'dashboard_view',
+                'created_at' => '2019-09-19 12:14:15',
+                'updated_at' => '2019-09-19 12:14:15',
+            ],
+            [
+                'id'         => '2',
+                'title'      => 'dashboard_edit',
+                'created_at' => '2019-09-19 12:14:15',
+                'updated_at' => '2019-09-19 12:14:15',
+            ],      
+        ];
+        Permission::insert($permissions);
 
-        \App\Models\User::factory(10)->create();
+        //Roles
+        $roles = [
+            [
+                'id'         => 1,
+                'title'      => 'Admin',
+                'created_at' => '2019-09-19 12:08:28',
+                'updated_at' => '2019-09-19 12:08:28',
+            ],
+        ];
+        Role::insert($roles);
+
+        //PermissionsRoles
+        $admin_permissions = Permission::all();
+        Role::findOrFail(1)->permissions()->sync($admin_permissions->pluck('id'));
+
+        //UserTables
+        $users = [
+            [
+                'id'             => 1,
+                'name'           => 'Admin',
+                'email'          => 'admin@admin.com',
+                'password'       => bcrypt('password'),
+                'remember_token' => null,
+                'created_at'     => '2019-09-19 12:08:28',
+                'updated_at'     => '2019-09-19 12:08:28',
+            ],
+        ];
+
+        User::insert($users);
+
+        //rolesusers
+        User::findOrFail(1)->roles()->sync(1);
+
+
+        //Other
+        User::factory(10)->create();
+
+
+
+
+
+
+
+
 
 
     }
