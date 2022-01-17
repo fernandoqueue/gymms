12d9e036000a93ddd0e54ec3bdfabcd585095e5a
fernandoqueue <fernando_q89@yahoo.com>
2022-01-17 14:29:03 -0800

    Added Roles and Permission to the tenant

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
- //       \App\Models\User::factory(10)->create();
+        $this->call([
+            Tenant\PermissionsTableSeeder::class,
+            RolesTableSeeder::class,
+            PermissionRoleTableSeeder::class,
+            UsersTableSeeder::class,
+            RoleUserTableSeeder::class,
+        ]);
+
     }
 }
--- database/seeders/tenant/DatabaseSeeder.php
+++ database/seeders/tenant/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            PermissionsTableSeeder::class,
+            Tenant\PermissionsTableSeeder::class,
             RolesTableSeeder::class,
             PermissionRoleTableSeeder::class,
             UsersTableSeeder::class,
