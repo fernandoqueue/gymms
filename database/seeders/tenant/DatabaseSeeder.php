fef398601b1e683b92f5e74bab95884cf6a0fbb7
fernandoqueue <fernando_q89@yahoo.com>
2022-01-17 14:55:42 -0800

    Added Roles and Permission to the tenant

--- database/seeders/tenant/DatabaseSeeder.php
+++ database/seeders/tenant/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            '\Database\Seeders\Tenant\PermissionsTableSeeder',
             PermissionsTableSeeder::class,
             RolesTableSeeder::class,
             PermissionRoleTableSeeder::class,
