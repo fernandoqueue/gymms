54983e61f48555ec162e8597c0044756762801a3
fernandoqueue <fernando_q89@yahoo.com>
2022-01-17 15:00:31 -0800

    Added Roles and Permission to the tenant

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,7 +36,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\AuthGates::class,
 
         ],
         'web' => [
@@ -47,6 +46,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\AuthGates::class,
 
         ],
 
